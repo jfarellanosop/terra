ea1f91bf5c6bcfdf844e62395b80ca1819e558e5
VSSarango <sofia.sarango@gmail.com>
2020-07-03 15:10:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,20 @@
-data "ibm_compute_bare_metal" "bare_metal" {
+/*data "ibm_compute_bare_metal" "bare_metal" {
   hostname    = "sourcepoc1"
   domain      = "sopra.cloud"
   most_recent = true
-}
-/*
-resource "ibm_compute_bare_metal" "hourly-bm1" {
-  hostname             = "hourly-bm1"
-  domain               = "sopra.cloud"
+}*/
+resource "ibm_compute_bare_metal" "test-terraform" {
+  hostname             = "test-poc_terraform"
+  domain               = ""
   os_reference_code    = "REDHAT_7_64"
   datacenter           = "ams03"
   network_speed        = 100   # Optional
   hourly_billing       = true  # Optional
   private_network_only = false # Optional
-  fixed_config_preset  = "S1270V6_16GB_1X800GB_SATA_NORAID"
-
+  fixed_config_preset  = "1U_6248S_384GB_1X3_8_SED_JBOD"
   tags = [
     "collectd",
     "mesos-master",
   ]
   notes = "note test"
 }
-*/
