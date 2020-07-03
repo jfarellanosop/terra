106c552ce1e15adcf58be9f7999d2d01ea9e120c
VSSarango <sofia.sarango@gmail.com>
2020-07-03 23:21:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,17 +6,21 @@
 resource "ibm_compute_bare_metal" "test-terraform" {
   hostname             = "test-poc_terraform"
   domain               = "jose-francisco-arellano-s-account.cloud"
-  #os_reference_code    = "CENTOS_6_32"
-  softwareLicense": {
-      "id": 17796,
-      "softwareDescription": {
-          "manufacturer": "Redhat",
-          "name": "EL for SAP Business Applications",
-          "referenceCode": "REDHAT_7_64",
-          "version": "7.7-64 for SAP Business Applications"
-      },
-      "softwareDescriptionId": 2640
-   }
+  memoryCapacity       = 32
+  #os_reference_code    = "REDHAT_7_64"
+  os_reference_code    = {
+    "manufacturerLicenseInstance": "D588C989-0334-4FAD-993E-D37807B62FF7",
+    "softwareLicense": {
+        "id": 17796,
+        "softwareDescription": {
+            "manufacturer": "Redhat",
+            "name": "EL for SAP Business Applications",
+            "referenceCode": "REDHAT_7_64",
+            "version": "7.7-64 for SAP Business Applications"
+        },
+       "softwareDescriptionId": 2640
+    }
+  }  
   datacenter           = "ams03"
   network_speed        = 100   # Optional
   hourly_billing       = true  # Optional
