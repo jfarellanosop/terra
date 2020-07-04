d7110b957a171798d22423a6d9622f23bad30535
VSSarango <sofia.sarango@gmail.com>
2020-07-04 22:27:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,19 @@
 }*/
  
 resource "ibm_compute_bare_metal" "test-terraform" {
-  hostname             = "test-poc_terraform"
-  ipv6_enabled         = false
+
+  datacenter           = "ams03"
   domain               = "jose-francisco-arellano-s-account.cloud"
+  hostname             = "test-poc_terraform"
+  hourly_billing       = true  # Optional 
+  ipv6_enabled         = false
   memory               = 32
+  network_speed        = 1000   # Optional
   os_reference_code    = "REDHAT_7_64"
+  private_network_only = false # Optional
   redundant_network      = true
   redundant_power_supply = true
-  datacenter           = "ams03"
-  network_speed        = 1000   # Optional
-  hourly_billing       = true  # Optional
-  private_network_only = false # Optional
-  fixed_config_preset  = "1U_6248S_384GB_1X3_8_SED_JBOD"
-  unbonded_network     = false  
+  unbonded_network       = false  
   tags = [
     "collectd",
     "mesos-master",
