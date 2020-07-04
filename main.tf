802a6041a3020a21dc5eb19971a5780f8541fea8
VSSarango <sofia.sarango@gmail.com>
2020-07-04 22:16:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,11 +10,10 @@ resource "ibm_compute_bare_metal" "test-terraform" {
   domain               = "jose-francisco-arellano-s-account.cloud"
   memory               = 32
   os_reference_code    = "REDHAT_7_64"
-  public_bandwidth     = 0  
   redundant_network      = true
   redundant_power_supply = true
   datacenter           = "ams03"
-  network_speed        = 10000   # Optional
+  network_speed        = 1000   # Optional
   hourly_billing       = true  # Optional
   private_network_only = false # Optional
   fixed_config_preset  = "1U_6248S_384GB_1X3_8_SED_JBOD"
