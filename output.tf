6b322c5f36dcc46466fa2e6bdc03486a415d1768
VSSarango <sofia.sarango@gmail.com>
2020-07-04 02:00:18 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "so" {
-  value = "http://${ibm_compute_bare_metal.bare_metal.os_reference_code}"
+  value = data.ibm_compute_bare_metal.bare_metal.os_reference_code
 }
 
 output "id" {
-  value = "http://${ibm_compute_bare_metal.bare_metal.id}"
+  value = data.ibm_compute_bare_metal.bare_metal.id
 }
