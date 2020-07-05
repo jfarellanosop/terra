68b3e73d4d0a076905c76fadd87e89ae0bd1ec0b
VSSarango <sofia.sarango@gmail.com>
2020-07-05 11:17:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ resource "ibm_compute_bare_metal" "test-terraform" {
   hostname             = "test-poc_terraform"
   hourly_billing       = true  # Optional 
   ipv6_enabled         = false
-  #cores                = 1
   memory               = 32
   network_speed        = 1000   # Optional
   os_reference_code    = "REDHAT_7_64"
@@ -20,7 +19,6 @@ resource "ibm_compute_bare_metal" "test-terraform" {
   redundant_power_supply = true
   unbonded_network       = false
   fixed_config_preset  = "1U_6248S_384GB_1X3_8_SED_JBOD"
-  user_metadata = "{\"value\":\"newvalue\"}" # Optional
   tags = [
     "collectd",
     "mesos-master",
