808d0d0faf6506d5f63746320de6dfaffbef7e99
jfarellanosop <66836048+jfarellanosop@users.noreply.github.com>
2020-07-06 16:45:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "ibm_compute_bare_metal" "test-terraform" {
  ipv6_enabled            = false
  memory                  = 32
  network_speed           = 100
- os_reference_code       = "OS_RHEL_FOR_SAP_BUSINESS_APPLICATION_7_X_64_BIT"
+ os_reference_code       = "REDHAT_7_64"
  private_network_only    = false
  public_bandwidth        = 0
  redundant_network       = true
