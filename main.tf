1e90516c5f2cf31885b843718e3bf50dd77a444d
VSSarango <sofia.sarango@gmail.com>
2020-07-06 09:56:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@
   domain      = "jose-francisco-arellano-s-account.cloud"
   most_recent = true
 }*/
-/*FUNCIONA PERO NO DESPLIEGA NADA*/
 resource "ibm_compute_bare_metal" "test-terraform" {
   datacenter           = "ams03"
   domain               = "jose-francisco-arellano-s-account.cloud"
@@ -12,11 +11,11 @@ resource "ibm_compute_bare_metal" "test-terraform" {
   ipv6_enabled         = false
   memory               = 32
   network_speed        = 1000   # Optional
+  os_reference_code    = "REDHAT_7_64"
   private_network_only = false # Optional
   redundant_network      = true
   redundant_power_supply = true
   unbonded_network       = false
-  os_reference_code    = "REDHAT_7_64"
   fixed_config_preset  = "1U_6248S_384GB_1X3_8_SED_JBOD"
   tags = [
     "collectd",
