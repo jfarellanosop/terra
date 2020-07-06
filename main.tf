8275fc4aedc64c1b47865dc6e0e2b1fd8abeb6a3
VSSarango <sofia.sarango@gmail.com>
2020-07-06 10:45:36 +0200

    Update main.tf
    Se añade codigo obtenido del data

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-data "ibm_compute_bare_metal" "bare_metal" {
+/*data "ibm_compute_bare_metal" "bare_metal" {
   hostname    = "arepoc1"
   domain      = "jose-francisco-arellano-s-account.cloud"
   most_recent = true
-}
+}*/
   /*
+  Antiguo
 resource "ibm_compute_bare_metal" "test-terraform" {
   datacenter           = "ams03"
   domain               = "jose-francisco-arellano-s-account.cloud"
@@ -25,4 +26,20 @@ resource "ibm_compute_bare_metal" "test-terraform" {
   os_reference_code    = "REDHAT_7_64"
 }
 */
-  
+resource "ibm_compute_bare_metal" "test-terraform" {
+ datacenter              = "ams03"
+ domain                  = "jose-francisco-arellano-s-account.cloud"
+ global_identifier       = "07540a12-740d-4b85-b460-bec217de221f"
+ hostname                = "createwithterraform"
+ hourly_billing          = true
+ ipv6_enabled            = false
+ memory                  = 32
+ most_recent             = true
+ network_speed           = 10000
+ os_reference_code       = "REDHAT_7_64"
+ private_network_only    = false
+ public_bandwidth        = 0
+ redundant_network       = true
+ redundant_power_supply  = true
+ unbonded_network        = false
+}
