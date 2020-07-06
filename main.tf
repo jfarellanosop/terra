27ec5ac1082213f72bcb8ec8be7065c3fb0260b7
VSSarango <sofia.sarango@gmail.com>
2020-07-06 09:44:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ resource "ibm_compute_bare_metal" "test-terraform" {
   datacenter           = "ams03"
   domain               = "jose-francisco-arellano-s-account.cloud"
   hostname             = "test-poc_terraform"
-  os_reference_code    = "REDHAT_7_64"
   hourly_billing       = true  # Optional 
   ipv6_enabled         = false
   memory               = 32
@@ -17,6 +16,7 @@ resource "ibm_compute_bare_metal" "test-terraform" {
   redundant_network      = true
   redundant_power_supply = true
   unbonded_network       = false
+  os_reference_code    = "REDHAT_7_64"
   fixed_config_preset  = "1U_6248S_384GB_1X3_8_SED_JBOD"
   tags = [
     "collectd",
