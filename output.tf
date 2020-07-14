80ee25a4861224ecfd57874bb72815da27e72346
jfarellanosop <66836048+jfarellanosop@users.noreply.github.com>
2020-07-14 12:42:53 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,4 @@
 output "os" {
-  value = data.ibm_compute_bare_metal.bare_metal.os_reference_code
-}
-
-output "id" {
-  value = data.ibm_compute_bare_metal.bare_metal.id
+  value = data.ibm_compute_bare_metal.test-terraform.os_reference_code
 }
 
